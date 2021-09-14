9aaa1189f184c19a70c98728b771f443cefc7151
김민재 <joshuara7235@gmail.com>
2021-09-14 19:23:06 +0900

    Array practice (index calculate)

--- basic_JS/Practice_JS/ArrayPractice.js
+++ basic_JS/Practice_JS/ArrayPractice.js
@@ -6,3 +6,9 @@ let datas = arrays.filter(num => parseInt(num) % 2 == 0);
 
 datas.map(value => console.log(value));
 
+
+let numbersArray = [2, 3, 4, 5, 6, 7];
+
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(`${i} 번째 값 : ${numbersArray[i]}`);
+}
