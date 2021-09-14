4ddffc462d30b11196d714860d8ac97f28c4ac27
김민재 <joshuara7235@gmail.com>
2021-09-14 19:55:48 +0900

    Obejct review for pracitce const for loop

--- basic_JS/Practice_JS/ObjectPractice.js
+++ basic_JS/Practice_JS/ObjectPractice.js
@@ -36,3 +36,38 @@ console.log(practiceObject);
 let p01 = new personPractice('kim', 'min', 30, 'male');
 
 console.log(p01.attack('weapon'));
+
+let p02 = new personPractice('kim' , 'hye', 30, 'male');
+
+p02.love = function(someone) {
+    return someone + ' love';
+}
+
+let someone = function () {
+    return 'minjae';
+}
+
+console.log(p02.lastname + ' ' + p02.love(someone()));
+
+
+for (const key in p01) {
+    console.log(key);
+}
+
+console.log('----------')
+
+for (const key in p02) {
+    console.log(key);
+    
+}
+
+// console.log('p01 : ' + p01.love);  //undefined
+// console.log('p02 : ' + p02.love('element')); //
+
+
+
+
+// console.log(p02.love = function () {
+//     return 'love';
+// })
+
