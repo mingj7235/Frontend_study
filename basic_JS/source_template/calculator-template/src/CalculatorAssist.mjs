b32c43f35cdf89dda28bd4d2e4fac96e5eea65ad
김민재 <joshuara7235@gmail.com>
2021-09-18 13:57:28 +0900

    Calculator made.. validation review needed

---
+++ basic_JS/source_template/calculator-template/src/CalculatorAssist.mjs
@@ -0,0 +1,19 @@
+import { question } from 'readline-sync';
+import calculator from './Calculator.mjs';
+
+function showPrompt () {
+    while(true) {
+        let inputData = question('Enter number or Operation : ');
+
+        if(inputData !== '=') {
+            calculator.appendData(inputData);
+            console.log(calculator.getInputDatasMessage());
+        }else {
+            //result print ( = 이 아닌경우므로)
+            console.log(calculator.getInputDatasMessage() + ' = ' + calculator.calculate());
+            break;
+        }
+    }
+}
+
+showPrompt();
