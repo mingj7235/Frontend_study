046c6176c095088bc1ff8ef6e8c9e983d53db2a6
김민재 <joshuara7235@gmail.com>
2021-09-21 15:54:17 +0900

    Entity class setting for setting

---
+++ basic_JS/source_template/travel-club-template/src/entity/TravelClub.mjs
@@ -0,0 +1,37 @@
+class TravelClub {
+
+    //클래스의 구성요소
+
+    //1. 데이터 속성 
+    //2. 메소드
+    //3. 초기화 메소드 (Initialized constructor) -> 생성자
+
+    constructor(name, intro) {
+        this._name = name
+        this._intro = intro;
+        this._foundedDate = new Date();
+    }
+
+    get name () {
+        return this._name;
+    }
+
+    set name (name) {
+        this._name = name;
+    }
+    
+    get intro () {
+        return this._intro;
+    }
+
+    set intro (intro){
+        this._intro = intro
+    }
+}
+
+let newClub = new TravelClub('Test club', 'Test intro');
+console.log(newClub.name);
+console.log(newClub);
+
+newClub.name = 'New name'; //setter 사용
+console.log(newClub);
